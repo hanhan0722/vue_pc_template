922cbbfc0ebeb159adef35fcccc17d9c06fdbb94
zhangyuhan <zhangyuhan0722@sina.com>
2019-12-22 19:18:07 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './vuex/store'
+import axios from 'axios'
+import { Loading , Message } from 'element-ui'
+
+Vue.use(Loading.directive)
+
+Vue.prototype.$ajax = axios
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$message = Message;
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/vuex/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+      
+  },
+  // 修改vuex中数据示例  
+  // this.$store.commit("newXXX",val); 
+  // 括号里前面是mutations里的方法名 后面是要改成的值
+  mutations: {}
+})
+
+export default store
